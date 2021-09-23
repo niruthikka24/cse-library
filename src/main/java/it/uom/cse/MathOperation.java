ea6359d4d79137fdf8b4c298fdb42b53bb7f9fac
hasiniV19 <83814896+hasiniV19@users.noreply.github.com>
2021-09-23 12:17:38 +0530

    created sumOfPrimes

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -5,6 +5,23 @@ import java.util.Arrays;
 
 public class MathOperation {
 
+    public static int sumOfPrimes(int n){
+        int sum =0;
+        for(int i = 2; i < n; i++){
+            boolean isPrime = true;
+            for(int j = 2; j < n - 1; j++){
+                if(i %j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
     public static double add(double... operands)
     {
         return Arrays.stream(operands).reduce(0d, Double::sum);
